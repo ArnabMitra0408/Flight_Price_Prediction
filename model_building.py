255e01e4b803250bb837eb40bf719e6cf333f0a8
Arnab Mitra <arnabmitra490@gmail.com>
2024-04-30 11:25:57 +0530

    Started Model Building

---
+++ model_building.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime, timedelta
+
+data=pd.read_csv("Data/Flight_Data_Processed.csv")
+
+
+
